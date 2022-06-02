0ab9c8ba342b0f78630d3b922110c0d11ec45d34
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-02 15:32:30 +0300

    Soru 1

--- Soru1/Program.cs
+++ Soru1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Soru1 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Pozitif Bir Tam Sayi Giriniz: ");
+            int n = int.Parse(Console.ReadLine());
+            String sonuc = "";
+            if (n<0)
+            {
+                Console.WriteLine("Girmiş olduğunuz sayı pozitif değildir.");
+
+            }
+            else
+            {
+                for(int i=1; i<=n; i++)
+                {
+                Console.Write("Lütfen"+ " " + i + "." + " " + "sayiyi giriniz: ");
+                int k = int.Parse(Console.ReadLine());
+
+                    if(k%2==0) 
+                    {
+                        sonuc = sonuc + Convert.ToString(k) + " ";
+                    }
+                
+                }
+                Console.WriteLine("Çift Sayılar: " + sonuc);
+            }
+
+        }
+    }
+}
