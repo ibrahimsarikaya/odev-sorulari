a9b1650b79a5abb55fae05d1cff3b46165872314
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-02 18:12:10 +0300

    Soru 4

---
+++ Soru4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Soru4 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Lütfen Bir Cümle Yaziniz: ");
+           string cumle = Console.ReadLine();
+           string[] kelimeler = cumle.Split(' ');
+           cumle = cumle.Replace(" ","");
+           Console.WriteLine("Toplam harf sayisi: " + cumle.Length + " " + "Toplam kelime Sayisi: " + kelimeler.Length);
+        }
+    }
+}
