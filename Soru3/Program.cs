5b8c9af8956ade450ff7698ab66078c17d229035
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-02 17:56:36 +0300

    Soru 3

---
+++ Soru3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Soru3 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Lütfen Pozitif Bir Tam Sayi Giriniz: ");
+           int n = int.Parse(Console.ReadLine());
+           String[] kelimeler = new String[n];
+
+           for (int i = 0; i < n; i++)
+           {
+               Console.Write("Lütfen " + i + "." +"kelimeyi yaziniz: ");
+               kelimeler[i]= Console.ReadLine();
+           }
+           foreach (String item in kelimeler.Reverse())
+           {
+               
+               Console.Write(item + " ");
+           }
+
+           
+            
+        }
+    }
+}
