b816f7a95dc4d0457c3108773f9a9eea4b79066d
ibrahimsarikaya <ibo7534@gmail.com>
2022-06-02 15:41:06 +0300

    Soru 2

---
+++ Soru2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Soru2 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
